15487c773e162ff4398728800899553fd28b5c4e
Pankaj Kant Patel <pkpdeveloper@gmail.com>
2020-01-06 21:18:56 +0530

    Create TreeNode and test cases

---
+++ app/src/androidTest/java/com/kotlin/graphassignment/TreeInstrumentedTest.kt
@@ -0,0 +1,79 @@
+package com.kotlin.graphassignment
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.kotlin.graphassignment.tree.Node
+import com.kotlin.graphassignment.tree.TreeNode
+import com.kotlin.graphassignment.tree.processJsonArray
+import com.kotlin.graphassignment.utils.JsonUtils
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertNotNull
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class TreeInstrumentedTest {
+    private val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+
+    @Test
+    fun treeInitTest() {
+        val treeNode = TreeNode<String, Int>("This is a string at ROOT LO", 0)
+        assertNotNull(treeNode)
+        assertEquals(0, treeNode.getChildren().size)
+
+        treeNode.addChild(TreeNode("This is the first level 1 string", 1))
+        assertEquals(1, treeNode.getChildren().size)
+
+        val nodeList = mutableListOf<TreeNode<String, Int>>()
+        nodeList.add(TreeNode("This is the first level 2 string", 2))
+        nodeList.add(TreeNode("This is the second level 2 string", 2))
+        treeNode.addChildren(nodeList, TreeNode("root L1", 1))
+
+        assertEquals(3, treeNode.getChildren().size)
+    }
+
+    @Test
+    fun treeJsonToTreeNodeInputTest() {
+
+        val json = JsonUtils.loadJSONFromAsset(appContext, "input.json")
+        assertNotNull(json)
+        val level = 0
+        val treeNode = Node(json.getString("uiValue"), level)
+        assertNotNull(treeNode)
+
+        val children = json.getJSONArray("children")
+        assertNotNull(children)
+
+        //process json data and add it to treeNode
+        treeNode.processJsonArray(children, level)
+
+        // Test root node
+        testNodeExists(treeNode, "This is a string at ROOT level", 0)
+
+        // Test first L1 node
+        testNodeExists(treeNode, "This is the first level 1 string", 1)
+
+        // Test second L1 node
+        testNodeExists(treeNode, "This is the second level 1 string", 1)
+
+        // Test first L2 node
+        testNodeExists(treeNode, "This is the first level 2 string", 2)
+
+        // Test second L2 node
+        testNodeExists(treeNode, "This is the second level 2 string", 2)
+
+        // Test first L3 node
+        testNodeExists(treeNode, "This is the first level 3 string", 3)
+
+        // Test second L3 node
+        testNodeExists(treeNode, "This is the second level 3 string", 3)
+
+    }
+
+    private fun testNodeExists(rootNode: Node, expectedValue: String, expectedLevel: Int) {
+        val resultNode = rootNode.find(expectedValue, expectedLevel)
+        assertEquals(expectedValue, resultNode?.getValue())
+        assertEquals(expectedLevel, resultNode?.getLevel())
+    }
+}
---
+++ app/src/main/java/com/kotlin/graphassignment/tree/TreeNode.kt
@@ -0,0 +1,86 @@
+package com.kotlin.graphassignment.tree
+
+import org.json.JSONArray
+import org.json.JSONObject
+
+typealias Node = TreeNode<String, Int>
+
+class TreeNode<T, Int>(private val value: String, private val level: Int) {
+    private var parent: TreeNode<T, Int>? = null
+
+    private var children = mutableSetOf<TreeNode<T, Int>>()
+
+    fun addChild(node: TreeNode<T, Int>) {
+        children.add(node)
+        node.parent = this
+    }
+
+    fun addChildren(nodeList: List<TreeNode<T, Int>>, element: TreeNode<T, Int>) {
+        nodeList.forEach {
+            children.add(it)
+        }
+        element.parent = this
+    }
+
+    override fun toString(): String {
+        var s = "value = $value and level = $level"
+        if (children.isNotEmpty()) {
+            s += " {" + children.map { it.toString() } + " }"
+        }
+        return s
+    }
+
+    fun getChildren(): MutableSet<TreeNode<T, Int>> {
+        return children
+    }
+
+    fun getValue(): String {
+        return value
+    }
+
+    fun getLevel(): Int {
+        return level
+    }
+
+    fun find(value: String, level: Int): TreeNode<T, Int>? {
+        if (this.level == level && this.value == value) {
+            return this
+        }
+
+        //recursive find node
+        this.getChildren().forEach {
+            findRecursiveNode(it, value, level)?.let {
+                return it
+            }
+        }
+        return null
+    }
+
+    private fun findRecursiveNode(
+        node: TreeNode<T, Int>,
+        value: String,
+        level: Int
+    ): TreeNode<T, Int>? {
+        if (node.level == level && node.value == value) {
+            return node
+        }
+        return null
+    }
+}
+
+fun Node.processJsonArray(jsonArray: JSONArray, level: kotlin.Int) {
+    for (index in 0 until jsonArray.length()) {
+        when (val json = jsonArray[index]) {
+            is JSONObject -> processJsonObject(json, level + 1)
+            is JSONArray -> processJsonArray(json, level + 1)
+
+        }
+    }
+}
+
+private fun Node.processJsonObject(
+    jsonObject: JSONObject,
+    level: Int
+) {
+    addChild(TreeNode(jsonObject.getString("uiValue"), level))
+}
