d4c0a1d3f797276922f7cdbae39e0a35e9f843bf
Pankaj Kant Patel <pkpdeveloper@gmail.com>
2020-01-07 15:09:25 +0530

    Add ButterKnife library and simple Home UI

--- app/src/main/java/com/kotlin/graphassignment/MainActivity.kt
+++ app/src/main/java/com/kotlin/graphassignment/MainActivity.kt
@@ -2,11 +2,26 @@ package com.kotlin.graphassignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextSwitcher
+import butterknife.BindView
+import butterknife.ButterKnife
 
 class MainActivity : AppCompatActivity() {
+
+    @BindView(R.id.btnPrevious)
+    lateinit var btnPrevious: Button
+
+    @BindView(R.id.btnNext)
+    lateinit var btnNext: Button
+
+    @BindView(R.id.textSwitcher)
+    lateinit var textSwitcher: TextSwitcher
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ButterKnife.bind(this)
     }
 }
